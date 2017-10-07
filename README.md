659d220f503f0907693a8d10b4e4c92517bca9f1
weybansky <weybansky@gmail.com>
2017-10-07 22:28:03 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# online-quiz
+A simple online quiz website developed with Vanila PHP
---
+++ template.php
@@ -0,0 +1,17 @@
+<!-- Default variables -->
+<?php require("../include/variables.php"); ?>
+<?php
+  // Declaring Custom Variable
+  $pageName = "Template";
+?>
+<!-- The Header -->
+<?php include("../include/header.php"); ?>
+
+<div class="container">
+  <div class="row">
+    Template
+  </div>
+</div>
+
+<!-- The Footer -->
+<?php include("../include/footer.php"); ?>
