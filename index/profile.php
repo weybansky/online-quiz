7877abd695615c31476453246a42de47c6537784
weybansky <weybansky@gmail.com>
2017-10-08 22:20:32 +0100

    changes

---
+++ index/profile.php
@@ -0,0 +1,15 @@
+<!-- Displaying Profile details -->
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <ul class="list-group">
+        <li class="list-group-item-info">Profile text</li>
+        <li class="list-group-item-info">Profile text</li>
+        <li class="list-group-item-info">Profile text</li>
+        <li class="list-group-item-info">Profile text</li>
+        <li class="list-group-item-info">Profile text</li>
+        <li class="list-group-item-info">Profile text</li>
+      </ul>
+    </div>
+  </div>
+</div>
